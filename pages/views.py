37b25a85062f18ef1d74e40de0777b4e9694f77e
Azeez Lawal <codewithazeez@gmail.com>
2020-12-07 18:35:09 +0100

    improving view and adding nav

---
+++ pages/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+from .models import Page
+# Create your views here.
+
+
+def index(request, pagename):
+    pagename = '/' + pagename
+    pg = Page.objects.get(permalink=pagename)
+    context = {
+        'title': pg.title,
+        'content': pg.body_text,
+        'last_updated': pg.update_date,
+    }
+    # assert False
+    # return HttpResponse("<h1>The Meandco Homepage</h1>")
+    return render(request, 'pages/page.html', context)
