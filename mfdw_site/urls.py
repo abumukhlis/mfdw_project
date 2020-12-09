f662dce790bbafbc5291cba654275558b27c697b
Azeez Lawal <codewithazeez@gmail.com>
2020-12-09 21:32:03 +0100

    user registration and login

--- mfdw_site/urls.py
+++ mfdw_site/urls.py
@@ -13,12 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.views.generic.base import TemplateView
+from quotes.views import Register
 from django.contrib import admin
 from django.urls import include, path
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/success/', TemplateView.as_view(template_name="registration/success.html"), name='register-success'),
+    path('register/', Register.as_view(), name='register'),
     path('quote/', include('quotes.urls')),
+    path('', include('django.contrib.auth.urls')),
     path('', include('pages.urls')),
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,9 +1,14 @@
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
 # Create your views here.
+
 from .models import Quote
 from .forms import QuoteForm
 from pages.models import Page
@@ -27,6 +32,14 @@ class QuoteView(DetailView):
         context["page_list"] = Page.objects.all()
         return context
     
+class Register(CreateView):
+    template_name = 'registration/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('register-success')
+
+    def form_valid(self, form):
+        form.save()
+        return HttpResponseRedirect(self.success_url)
 
 def quote_req(request):
     submitted = False
@@ -41,3 +54,4 @@ def quote_req(request):
             submitted = True
 
     return render(request, 'quotes/quote.html', {'form': form, 'page_list': Page.objects.all(), 'submitted': submitted})
+
